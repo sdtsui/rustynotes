fabd11dcb473e4ab8f96fc411aebd58a05c59ce8
sdtsui <szehungdanieltsui@gmail.com>
2017-11-15 00:24:53 -0800

    init

---
+++ hello_world/hello.rs
@@ -0,0 +1,7 @@
+// https://rustbyexample.com/hello.html
+fn main() {
+  println!("Hello World!");
+  println!("I'm a Rustacean!");
+}
+// println is a macro
+// https://rustbyexample.com/macros.html
